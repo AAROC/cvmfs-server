7a14e409ee4570dc169e1097e926c581ea5a0adf
Bruce Becker <brucellino@gmail.com>
2017-12-15 11:56:31 +0100

    Add Repository tests (Red)

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,14 +1,42 @@
 import os
-
+import pytest
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
-
 def test_hosts_file(host):
+    """Basic sanity checks."""
     f = host.file('/etc/hosts')
 
     assert f.exists
     assert f.user == 'root'
     assert f.group == 'root'
+
+@pytest.mark.parametrize("name", [
+    ("cvmfs"),
+    ("cvmfs-server"),
+])
+
+def test_packages(host, name):
+    
+    pkg = host.package(name)
+    assert pkg.is_installed
+
+@pytest.mark.parametrize("dir", [
+    ("/var/spool/cvmfs"),
+    ("/srv/cvmfs"),
+    ("/cvmfs"),
+    ("/etc/cvmfs"),
+    ("/etc/cvmfs/repositories.d"),
+])
+
+
+def test_directories(host, dir):
+    """
+    Check that the relevant directories are present.
+    See http://cvmfs.readthedocs.io/en/stable/cpt-repo.html#sct-serveranatomy
+    """
+
+    path = host.file(dir)
+    assert path.is_directory
