167354fff98ddfeb4ee1a0a5bd4a9fa399146037
Bruce Becker <brucellino@gmail.com>
2017-12-15 13:36:50 +0100

    Fix linter errors in tests

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,10 +1,13 @@
 import os
+
 import pytest
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
+
+# Basic sanity tests.
 def test_hosts_file(host):
     """Basic sanity checks."""
     f = host.file('/etc/hosts')
@@ -13,30 +16,38 @@ def test_hosts_file(host):
     assert f.user == 'root'
     assert f.group == 'root'
 
-@pytest.mark.parametrize("name", [
-    ("cvmfs"),
-    ("cvmfs-server"),
-])
 
+# Set some fixtures for the packages that have to be installed on a CVMFS
+# server. See defaults/main.yml -> cvmfs_packages
+@pytest.mark.parametrize('name', [
+    ('cvmfs'),
+    ('cvmfs-server'),
+])
 def test_packages(host, name):
-    
+    """
+    Check that the relevant packages are installed.
+
+    See http://cvmfs.readthedocs.io/en/stable/cpt-repo.html#installation
+    """
     pkg = host.package(name)
     assert pkg.is_installed
 
-@pytest.mark.parametrize("dir", [
-    ("/var/spool/cvmfs"),
-    ("/srv/cvmfs"),
-    ("/cvmfs"),
-    ("/etc/cvmfs"),
-    ("/etc/cvmfs/repositories.d"),
+# Set some fixtures for the directories that have to be available on a CVMFS
+# server.
+
+
+@pytest.mark.parametrize('dir', [
+    ('/var/spool/cvmfs'),
+    ('/srv/cvmfs'),
+    ('/cvmfs'),
+    ('/etc/cvmfs'),
+    ('/etc/cvmfs/repositories.d'),
 ])
-
-
 def test_directories(host, dir):
     """
     Check that the relevant directories are present.
+
     See http://cvmfs.readthedocs.io/en/stable/cpt-repo.html#sct-serveranatomy
     """
-
     path = host.file(dir)
     assert path.is_directory
